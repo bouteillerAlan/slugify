ffb769f98394fbfc684f0d4636d8934bccd94787
Alan Bouteiller <30547569+bouteillerAlan@users.noreply.github.com>
2019-10-25 16:25:26 +0200

    :sparkles: add function

---
+++ slugify.js
@@ -0,0 +1,14 @@
+export function CleanSlugs(string: string) {
+    const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'; // bad char
+    const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnooooooooprrsssssttuuuuuuuuuwxyyzzz------'; // Replace by
+    const p = new RegExp(a.split('').join('|'), 'g'); // build the regex
+
+    return string.toString().toLowerCase()
+        .replace(/\s+/g, '-') // Replace spaces with -
+        .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+        .replace(/&/g, '-and-') // Replace & with 'and'
+        .replace(/[^\w\-]+/g, '') // Remove all non-word characters
+        .replace(/\-\-+/g, '-') // Replace multiple - with single -
+        .replace(/^-+/, '') // Trim - from start of text
+        .replace(/-+$/, '') // Trim - from end of text
+}
